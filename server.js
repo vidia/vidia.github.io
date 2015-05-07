9f8af0351fa48829cebe470613fa9b88363b4cdc
David Tschida <dmtschida1@gmail.com>
2015-05-07 16:54:24 -0400

    Added vhost to support lawsoncs.website domain

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 require("newrelic");
-require('harp').server(__dirname + "/public", { port: process.env.PORT || 5000 });
+var express = require("express");
+var vhost = require('vhost');
+var harp = require("harp");
+
+var main = express();
+main.use(express.static(__dirname + "/public"));
+main.use(harp.mount(__dirname + "/public"));
+
+var lawsoncs = express();
+lawsoncs.use(express.static(__dirname + "/public/cameras"));
+lawsoncs.use(harp.mount(__dirname + "/public/cameras"));
+
+
+var app = express();
+app.use(vhost('lawsoncs.webcam', lawsoncs)); // Serves all subdomains via Redirect app
+app.use(vhost('*.davidtschida.com', main));
+app.use(vhost('davidtschida.com', main));
+
+app.listen(process.env.PORT || 5000);
